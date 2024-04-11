6a85151b6d7db7c7460b523263ace98115ebf48f
rnogara <r.nogara.dev@gmail.com>
2024-04-11 18:53:30 -0300

    fix dropdown of button "More" on menu and add clients buttons

---
+++ naranja_labs_fullstack_jr/src/app/components/client.tsx
@@ -0,0 +1,12 @@
+export default function Client() {
+    return (
+        <div className="flex flex-row w-auto justify-between itens-center">
+            <button className="text_lg leading-10 m-0.5 mr-8 highlight-text underline">
+                Sign In
+            </button>
+            <button className="text_lg leading-10 m-0.5 w-36 h-14 pt-1 highlight-bg text-white rounded-full">
+                Sign Up
+            </button>
+        </div>
+    )
+}
--- naranja_labs_fullstack_jr/src/app/components/menu.tsx
+++ naranja_labs_fullstack_jr/src/app/components/menu.tsx
@@ -1,8 +1,9 @@
 export default function Menu() {
 
     const handleClick = () => {
-        const menu = document.querySelector('.menu');
-        menu && menu.classList.toggle('hidden');
+        const menu = document.getElementById('hidden-menu');
+        if (!menu) return console.error('Menu not found');
+        menu.classList.contains('hidden')? menu.classList.remove('hidden') : menu.classList.add('hidden');
     }
 
     return (
@@ -17,7 +18,7 @@ export default function Menu() {
             <svg className="-mr-1 ml-3 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                 <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" cap-rule="evenodd" />
             </svg>
-            <div className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none hidden" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex={-1}>
+            <div id="hidden-menu" className="absolute flex flex-col right-72 top-28 z-10 mt-2 w-56 origin-top-right rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none hidden" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex={-1}>
                 <a href="#" className="poppins text-lg mb-0.5 ml-3">Prices</a>
                 <a href="#" className="poppins text-lg mb-0.5 ml-3">Reviews</a>
                 <a href="#" className="poppins text-lg mb-0.5 ml-3">Our Team</a>
