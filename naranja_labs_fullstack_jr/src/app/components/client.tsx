6ec2d727c5ac51dbca79639ff06cc3abd338854b
rnogara <r.nogara.dev@gmail.com>
2024-04-12 09:31:49 -0300

    fix small size differences and add a text color on global.css

--- naranja_labs_fullstack_jr/src/app/components/client.tsx
+++ naranja_labs_fullstack_jr/src/app/components/client.tsx
@@ -1,10 +1,10 @@
 export default function Client() {
     return (
-        <div className="flex flex-row w-auto justify-between itens-center">
-            <button className="text_lg leading-10 m-0.5 mr-8 highlight-text underline">
+        <div className="flex flex-row w-[253px] -ml-16 justify-between itens-center">
+            <button className="text_xl leading-10 m-0.5 highlight-text underline">
                 Sign In
             </button>
-            <button className="text_lg leading-10 m-0.5 w-36 h-14 pt-1 highlight-bg text-white rounded-full">
+            <button className="text_xl leading-10 m-0.5 w-[149px] h-[60px] pt-1 highlight-bg text-white rounded-full">
                 Sign Up
             </button>
         </div>
--- naranja_labs_fullstack_jr/src/app/components/contact.tsx
+++ naranja_labs_fullstack_jr/src/app/components/contact.tsx
@@ -8,17 +8,17 @@ import linkedin from '../../assets/icons/Linkedin.svg';
 export default function Contact() {
     return (
         <section className='w-full flex flex-row mt-4 mb-4 justify-between'>
-            <div className='flex flex-row w-auto h-7 justify-between itens-center'>
-                <PhoneIcon className='highlight-text h-5 w-5 m-0.5 stroke-2' />
-                <p className='poppins text-lg mb-0.5 ml-3'>+12 345 6789 0</p>
-                <EnvelopeIcon className='highlight-text h-5 w-5 m-0.5 stroke-2 ml-8' />
-                <p className='poppins text-lg mb-0.5 ml-3'>support@homie.com</p>
+            <div className='flex flex-row w-auto h-7 justify-between itens-end'>
+                <PhoneIcon className='highlight-text h-6 w-6 m-0.5 stroke-2' />
+                <p className='poppins text-[18px]/[27px] mb-0.5 mt-[3px] ml-3'>+12 345 6789 0</p>
+                <EnvelopeIcon className='highlight-text h-6 w-6 m-0.5 stroke-2 ml-8' />
+                <p className='poppins text-[18px]/[27px] mb-0.5 mt-[3px] ml-3'>support@homie.com</p>
             </div>
             <div className='flex flex-row w-48 h-7 justify-between itens-center'>
-                <Image src={instagram} alt='instagram logo' className='h-5 w-5 m-0.5' />
-                <Image src={facebook} alt='facebook logo' className='h-5 w-5 m-0.5' />
-                <Image src={twitter} alt='twitter logo' className='h-5 w-5 m-0.5' />
-                <Image src={linkedin} alt='linkedin logo' className='h-5 w-5 m-0.5' />
+                <Image src={instagram} alt='instagram logo' className='h-6 w-6 m-0.5' />
+                <Image src={facebook} alt='facebook logo' className='h-6 w-6 m-0.5' />
+                <Image src={twitter} alt='twitter logo' className='h-6 w-6 m-0.5' />
+                <Image src={linkedin} alt='linkedin logo' className='h-6 w-6 m-0.5' />
             </div>
         </section>
     )
